35e195b1978b1357623c060ff5ace63ebb061632
CarlosAlberto12134 <131511791+cabeto123@users.noreply.github.com>
2023-07-21 17:11:19 -0600

    Add files via upload

---
+++ Lab1P2_CarlosMoncada/src/lab1p2_carlosmoncada/Lab1P2_CarlosMoncada.java
@@ -0,0 +1,171 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab1p2_carlosmoncada;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author calol
+ */
+public class Lab1P2_CarlosMoncada {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int opcion = 0;
+        while (opcion != 3) {
+            System.out.println("1.Ejercicio 1");
+            System.out.println("2.Ejercicio 2");
+            System.out.println("3.Salir");
+            opcion = entrada.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    
+                    
+                break;
+                case 2:
+                    String numero="";
+                    System.out.println("Digite X: ");
+                    String XString =entrada.next();
+                    for (int i = 0; i < XString.length(); i++) {
+                    
+                    int codigoascii=(int)XString.charAt(i);
+                        if (codigoascii>=48&&codigoascii<=57) {
+                            numero+=XString.charAt(i);
+                        }
+                    }
+                    int X = Integer.parseInt(numero);
+                    
+                    
+                    System.out.println("Digite el limite: ");
+                    String xlimite =entrada.next();
+                    numero="";
+                    for (int i = 0; i < xlimite.length(); i++) {
+                    int codigoascii=(int)xlimite.charAt(i);
+                        if (codigoascii>=48&&codigoascii<=57) {
+                            numero+=xlimite.charAt(i);
+                        }
+                    }
+                    int limite=Integer.parseInt(numero);
+                    
+                    
+                    if (limite<0) {
+                        System.out.println("Limite incorrecto");
+                    }else{
+                    double resp=seno(limite,0,X,0);
+                    double respcoseno=coseno(limite,0,X,0);
+                        System.out.println("Seno: "+resp);
+                        System.out.println("Coseno: "+respcoseno);
+                        if (X<1.5706) {
+                            double resptangente= tangente(limite,0,X,0);
+                            System.out.println("Tangente "+resptangente);    
+                        }
+                    
+                    }
+                    
+                    break;
+                
+                    
+                    
+
+            }
+
+        }
+    }//fin del main
+
+    public static double seno(int limite, int n, double X,double operacion) {
+        if (n == limite+1) {
+        
+                
+            
+
+            return 0;
+        } else {
+            double numerador;
+            double denominador;
+            
+            double x;
+            x =  Math.pow(X, (2 * n) + 1);
+            
+            numerador =  Math.pow(-1, n);
+            
+            denominador = 2 * n + 1;
+            denominador = factorial(denominador);
+            operacion = numerador / denominador * x;
+            
+            
+            return operacion + (seno(limite, n + 1, X,operacion));
+
+        }
+
+    }
+    
+    public static double coseno(int limite, int n, double X,double operacion){
+        if (n==limite+1) {
+        return 0;    
+        }else{
+         double numerador;
+            double denominador;
+            
+            double x;
+            x =  Math.pow(X, 2 * n);
+            
+            numerador =  Math.pow(-1, n);
+            
+            denominador = 2 * n ;
+            denominador = factorial(denominador);
+            operacion = numerador / denominador * x;
+            System.out.println(operacion);
+            return operacion + (coseno(limite, n + 1, X,operacion));
+        
+        
+        }
+    
+    
+    }
+    
+    public static double tangente(int limite, int n, double X,double operacion){
+        if (n==limite+1) {
+        return 0;    
+        }else{
+         double numerador;
+            double denominador;
+            
+            double x;
+            x =  Math.pow(X, 2 * n-1);
+            
+            numerador =  Math.pow(2, n)*Math.pow(-4, n)*(1-Math.pow(4, n));
+            
+            denominador = 2 * n ;
+            denominador = factorial(denominador);
+            operacion = numerador / denominador * x;
+            
+            return operacion + (tangente(limite, n + 1, X,operacion));
+        
+        
+        }
+    
+    
+    }
+    
+    public static double factorial(double n) {
+        double aux = n;
+        if (n==0) {
+            return 1;
+        }else{
+        for (double i = n - 1; i >= 1; i--) {
+            aux *= i;
+        }
+        return aux;
+        }
+        
+        
+    }
+
+}
